745625d7c0f19979c0ed938f030df4c1b763eec2
leonovd-bit <leonovd@clarkson.edu>
2025-08-12 02:05:03 -0400

    Update Android build configuration - Enhanced Gradle configs for advanced features and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
@@ -12,31 +12,14 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
     
-    // Memory optimization configurations
-    packagingOptions {
-        pickFirst("**/libc++_shared.so")
-        pickFirst("**/libjsc.so")
-        exclude("META-INF/DEPENDENCIES")
-        exclude("META-INF/LICENSE")
-        exclude("META-INF/LICENSE.txt")
-        exclude("META-INF/NOTICE")
-        exclude("META-INF/NOTICE.txt")
-    }
-    
-    buildFeatures {
-        buildConfig = false
-        resValues = false
-        aidl = false
-        renderScript = false
-    }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,11 +18,14 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.3.0" apply false
+    // Ensure library plugins used by Flutter dependencies (e.g., flutter_local_notifications)
+    // use the same Android Gradle Plugin version as the app to avoid configuration errors.
+    id("com.android.library") version "8.3.0" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 include(":app")
